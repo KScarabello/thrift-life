b58be72355a377037235886ba9392f34913a7dc2
Kim Scarabello <kimberly.scarabello@gmail.com>
2017-11-17 01:41:13 -0600

    animations added to thrift life h1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ThriftLife from './components/ThriftLife';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="big-container">
+      <div>
         <div className="video-container">  
               <video preload='auto' loop='loop' muted='muted' autoPlay='true' className='video'>
                   <source src="https://www.dl.dropboxusercontent.com/s/wvkjh6acwcyucay/563400165.mp4?dl=0" type="video/mp4" />
                   Video Not Supported
               </video>
         </div>
-        <ThriftLife />
+        
+        <h1>Thrift Life</h1>
       </div>
     );
   }
