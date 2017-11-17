c8c9ec8c2731758f10e857e6aee1136cdc0b86a8
Kim Scarabello <kimberly.scarabello@gmail.com>
2017-11-17 00:52:41 -0600

    sass installed, thriftlife component created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ThriftLife from './components/ThriftLife';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="big-container">
+        <div className="video-container">  
+              <video preload='auto' loop='loop' muted='muted' autoPlay='true' className='video'>
+                  <source src="https://www.dl.dropboxusercontent.com/s/wvkjh6acwcyucay/563400165.mp4?dl=0" type="video/mp4" />
+                  Video Not Supported
+              </video>
+        </div>
+        <ThriftLife />
       </div>
     );
   }
---
+++ src/components/ThriftLife.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ThriftLife = () => {
+
+    return (
+
+        <div>
+            <h1>Thrift Life</h1>
+        </div>
+
+    )
+
+
+} 
+
+export default ThriftLife;
